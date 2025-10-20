1e042431859d6b014abe5f3c897fe1b7f0475fc6
GabyB73 <geb.beg.73@gmail.com>
2025-10-20 09:10:55 +0200

    Ejercicios del tema Introduction completados

--- Introduction/Default arguments/src/Task.kt
+++ Introduction/Default arguments/src/Task.kt
@@ -2,8 +2,8 @@ fun foo(name: String, number: Int, toUpperCase: Boolean) =
         (if (toUpperCase) name.uppercase() else name) + number
 
 fun useFoo() = listOf(
-        foo("a"),
-        foo("b", number = 1),
-        foo("c", toUpperCase = true),
+        foo("a", 42, false),
+        foo("b", 1, false),
+        foo("c", 42, true),
         foo(name = "d", number = 2, toUpperCase = true)
 )
